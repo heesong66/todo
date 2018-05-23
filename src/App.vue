151a6e55a3db8bfa237e34e04ce7b66068a69487
hsong <song@qubitsec.com>
2018-05-23 15:09:25 +0900

    application 생성 #vue init webpack-simple

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoHeader from './components/TodoHeader.vue'
+
+export default {
+  components:{
+    'TodoList': TodoList,
+    'TodoInput': TodoInput,
+    'TodoHeader': TodoHeader,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Poor Story', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
