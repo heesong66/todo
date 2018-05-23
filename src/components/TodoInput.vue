c2dc02abcec2919e36a451cf819cd325039a179d
hsong <song@qubitsec.com>
2018-05-23 15:10:01 +0900

    .vue components 생성

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem : ''
+        }
+    },
+
+    methods:{
+        addTodo() {
+            if(this.newTodoItem !== ''){
+                var tmp = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(tmp, tmp);
+                this.clearInput();
+            }else{
+                alert("input text!");
+            }
+        },
+        clearInput(){
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
