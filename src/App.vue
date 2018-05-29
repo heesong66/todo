e4f64d50a04243f7b25915f7d0a5c06970111184
hsong <song@qubitsec.com>
2018-05-29 14:08:41 +0900

    TODO List Application (vue)

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,66 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
   </div>
 </template>
 
 <script>
-import TodoFooter from './components/TodoFooter.vue'
-import TodoList from './components/TodoList.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoHeader from "./components/TodoHeader.vue";
 
 export default {
-  components:{
-    'TodoList': TodoList,
-    'TodoInput': TodoInput,
-    'TodoHeader': TodoHeader,
-    'TodoFooter': TodoFooter
+  data() {
+    return {
+      todoItems: [],
+      newTodoItem: ""
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  components: {
+    TodoList: TodoList,
+    TodoInput: TodoInput,
+    TodoHeader: TodoHeader,
+    TodoFooter: TodoFooter
+  },
+  methods: {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Poor Story', cursive;
+  font-family: "Poor Story", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
